c55ea74ef6b26da23fe55b5c89b7c90f9659151b
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2017-12-02 09:59:33 -0200

    Formatar em 100 colunas

--- tools/listar-recursos.py
+++ tools/listar-recursos.py
@@ -10,5 +10,4 @@ for image in images:
 print("\nListando sabores...")
 flavors = cloud.list_flavors()
 for flavor in flavors:
-    print("{}\t{} vcpus \t{} MB  \t{}".format(
-        flavor.id, flavor.vcpus, flavor.ram, flavor.name))
+    print("{}\t{} vcpus \t{} MB  \t{}".format(flavor.id, flavor.vcpus, flavor.ram, flavor.name))
