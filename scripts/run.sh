a4c88e8f9a26ad2d4ff2b484c49168bca5a372db
RODRIGO NEUMANN BARROS FERREIRA <rneumann@br.ibm.com>
2017-12-03 13:57:24 -0200

    Finalizar script de deploy

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
 echo "nameserver 9.9.9.9" >> /etc/resolv.conf;
+
+echo "Clonando o repositório..."
+git clone git@github.com:r3gh/hackathonRioHeatMap.git
+cd ~/hackathonRioHeatMap
+
+echo "Instalando dependências do sistema..."
+apt-get update
+apt-get install -y python3 python3-pip python3-dev build-essential postgresql-9.6
+
+echo "Instalando dependências do Python..."
+pip3 install --upgrade pip setuptools
+pip3 install --upgrade -r requirements.txt
+pip3 install --upgrade -r ott-crawler/requirements.txt
+
+echo "Iniciando componentes..."
+./run.sh
